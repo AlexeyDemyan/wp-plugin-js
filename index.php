32b8e8c6e082b74b28c4f613ace9e7d1a3759e82
Alexey Demyanov <deal110988@him-mail.ch>
2025-05-19 17:02:47 +0200

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+    Plugin name: Alex Attenion Quiz Plugin
+    Description: Adding multiple choice quiz to blog posts
+    Version: 1.0
+    Author: Alex
+    Author URI: https://github.com/AlexeyDemyan
+*/
+
+if (! defined('ABSPATH')) exit; // Exit if accessed directly
